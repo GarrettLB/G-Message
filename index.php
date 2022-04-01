a15649e536056c5f2f32125c4f5cfcbb60cc82d8
GLBryan <garrettleebryan@gmail.com>
2022-04-01 15:17:51 -0400

    large and mobile styling for login and signup pages

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>G-Message</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
-<body>
-  <form action="includes/signup.inc.php" method="POST">
+<body class="bod">
+
+  <form action="includes/signup.inc.php" method="POST" id="signupForm">
     <h2>Sign Up</h2>
     <label for="email">Email</label>  
     <input name="email" type="text">
@@ -22,8 +26,8 @@
     <label for="passwordRepeat">Re-Enter Password</label>
     <input name="passwordRepeat" type="password">
     <br>
-    <button name='submit' type="submit">Sign Up</button>
-    <a href="login.php">Or log in here</a>
+    <button class="formBtn" name='submit' type="submit">Sign Up</button>
+    <a class="aTag" href="login.php">Or log in here</a>
   </form>  
 
   <?php
@@ -42,6 +46,9 @@
       }
     }
   ?>
-  
+
+  <?php
+    include './includes/footer.inc.php'
+  ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -5,10 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>G-Message</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
-<body>
-  <form action="includes/login.inc.php" method="POST">
+<body class="bod">
+  <form action="includes/login.inc.php" method="POST" id="loginForm" >
     <h2>Log In</h2>
     <label for="usermail">Username or Email</label>  
     <input name="usermail" type="text">
@@ -16,8 +19,8 @@
     <label for="password">Password</label>
     <input name="password" type="password">
     <br>
-    <button name='submit' type="submit">Log In</button>
-    <a href="index.php">Or sign up here</a>  
+    <button class="formBtn" name='submit' type="submit">Log In</button>
+    <a class="aTag" href="index.php">Or sign up here</a>  
   </form>  
 
   <?php
@@ -32,6 +35,10 @@
         echo "<p>Invalid Credentials!</p>";
       }
     }
+  ?>
+
+  <?php
+    include './includes/footer.inc.php'
   ?>
 </body>
 </html>
